1d8c954e3e44e3b765705d6428ce61bc3cdd2c3b
Sven Gestegard Robertz <sven.robertz@cs.lth.se>
2022-01-18 11:46:41 +0100

    Extended leak example
    Added code so that the leaks not optimized away

--- lab1/buggy_programs/leak.cc
+++ lab1/buggy_programs/leak.cc
@@ -2,12 +2,23 @@
 using std::cout;
 
 struct Foo{
-    Foo(int s) :p{new int[s]} {}
+    Foo(int s) :sz{s}, p{new int[s]} {
+            for(int i=0; i < s; ++i) p[i]=i+1; // or use std:iota
+    }
+    void print() const {
+        cout << "Foo: ";
+        for(int i=0; i<sz; ++i){
+            cout << p[i] << " ";
+        }
+        cout << "\n";;
+    }
+    int sz{};
     int* p;
 };
 
 struct Bar{
-    Bar(int x) :f(x) {}
+    Bar(int s) :f(s) {};
+    void print() const {f.print();}
     Foo f;
 };
 
@@ -17,8 +28,10 @@ void example1()
     {
         cout << "entering inner block\n";
         Bar b(20);
+        b.print();
         cout << "leaving inner block\n";
     }
+    f.print();
     cout << "leaving example1\n";
 }
 
